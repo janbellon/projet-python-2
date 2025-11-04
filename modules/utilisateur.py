4263bd0be1d1b4170bdb8771451f7986fadb92d7
Jan Bellon <janski.bellon@gmail.com>
2025-11-04 14:35:22 +0100

    04/11

--- modules/utilisateur.py
+++ modules/utilisateur.py
@@ -1,11 +1,23 @@
 class Utilisateur:
-    def __init__(self):
-        pass
+    _id_counter = 1
+    def __init__(self, nom, email):
+        self.uuid : int = self._id_counter
+        self._id_counter += 1
+        self.nom : str = nom
+        self.email : str = email
 
 class Lecteur(Utilisateur):
-    def __init__(self):
-        pass
+    def emprunter(self, Livre):
+        Livre.add()
+    def rendre(self, Livre):
+        Livre.add()
 
 class Bibliothecaire(Utilisateur):
-    def __init__(self):
+    def ajouter(self, Livre):
+        
+    def modifier(self, Livre):
         pass
+    def supprimer(self, Livre):
+        pass
+
+utilisateur = Utilisateur(14, "bob")
