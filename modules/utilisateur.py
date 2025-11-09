a5632eee66890e249f3a3bed9aa29116b091f8b9
Jan BELLON <jbellon@MacBook-Pro-de-Jan.local>
2025-11-09 20:40:44 +0100

    Users corrections

--- modules/utilisateur.py
+++ modules/utilisateur.py
@@ -1,11 +1,66 @@
+from modules.livre import Livre
+import hashlib
+import time
+import base64
+
 class Utilisateur:
-    def __init__(self):
-        pass
+    _id_counter = 1
+
+    def generer_uuid(self):
+        uuid = Utilisateur._id_counter
+        Utilisateur._id_counter += 1
+        return uuid
 
 class Lecteur(Utilisateur):
-    def __init__(self):
-        pass
+    def __init__(self, nom, email, password):
+        self.role = 1
+        self.nom : str = nom
+        self.email : str = email
+        self.password : str = hashlib.sha256(password.encode())
+        self.uuid : int = self.generer_uuid()
+
+    def emprunter(self):
+        id_livre = int(input("\nID du livre à emprunter \n -> "))
+        # Vérifier que le livre est disponible
+        Livre.modifier(id_livre, statut="emprunte")
+
+    def rendre(self):
+        id_livre = int(input("\nID du livre à rendre \n -> "))
+        # Vérifier que le livre est emprunté par la personne
+        Livre.modifier(id_livre, statut="disponible")
 
 class Bibliothecaire(Utilisateur):
-    def __init__(self):
-        pass
+    def __init__(self, nom, email, password):
+        self.role = 2
+        self.nom : str = nom
+        self.email : str = email
+        self.password : str = hashlib.sha256(password.encode())
+        self.uuid : int = self.generer_uuid()
+
+    def ajouter(self):
+        titre = input(f"\nTitre du livre \n -> ")
+        auteur = input(f"\nAuteur \n ->")
+        categorie = input(f"\nCatégorie \n ->")
+        statut = input(f"\nStatut \n ->")
+        livre = Livre.add(titre, auteur, categorie, statut)
+        print(f"Livre ({livre.titre}, {livre.auteur}, {livre.categorie}, {livre.statut}) ajouté avec succès.")
+    
+    def modifier(self):
+        id_livre = int(input("\nID du livre à modifier \n -> "))
+        titre = input(f"\nTitre du livre ({Livre.Livre.titre}) (Entrée pour passer) \n -> ") or None
+        auteur = input(f"\nAuteur ({Livre.Livre.auteur}) (Entrée pour passer) \n ->") or None
+        categorie = input(f"\nCatégorie ({Livre.categorie}) (Entrée pour passer) \n ->") or None
+        statut = input(f"\nStatut ({Livre.statut}) (Entrée pour passer) \n ->") or None
+        Livre.modifier(id_livre, titre, auteur, categorie, statut)
+    
+    def supprimer(self):
+        id_livre = int(input("\nID du livre à supprimer \n -> "))
+        Livre.supp(id_livre)
+
+class Admin(Utilisateur):
+    def __init__(self, nom, email, password):
+        self.role = 99
+        self.nom : str = nom
+        self.email : str = email
+        self.password : str = hashlib.sha256(password.encode())
+        self.uuid : int = self.generer_uuid()
